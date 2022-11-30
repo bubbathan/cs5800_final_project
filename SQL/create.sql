8e13bf8193086aca73de94edf3d922f2ec44cd4b
Jonathan Hill <75814472+bubbathan@users.noreply.github.com>
2022-11-29 22:01:04 -0700

    Added relations to `create.sql` and made minor adjustments to the schema. Ready for review.

--- SQL/create.sql
+++ SQL/create.sql
@@ -7,9 +7,9 @@ DROP TABLE IF EXISTS
       STAFF
     , PATIENT
     , SHIFT
-    , PROCEDURE
+    , MPROCEDURE
     , DEPARTMENT
-    , BUILDING
+    , LOCATION
     , ROOM
     , MEDICATION
     , DIAGNOSIS
@@ -20,127 +20,233 @@ DROP TABLE IF EXISTS
     , NURSE
     , OUTPATIENTPROCEDURE
     , INPATIENTPROCEDURE
+    , WORKSIN
+    , WORKSWITH
+    , PRESCRIBES
+    , HOUSES
+    , ISSCHEDULED
+    , LOCATEDIN
+    , DIAGNOSED
+    , TREAT
+    , VISIT
+    , STAYS
 
-CREATE TABLE STAFF(
-    employeeId INT
-  , ssn           VARCHAR(11)
-  , employeeName  VARCHAR(100)
+CREATE TABLE STAFF (
+    employeeId    INT             NOT NULL
+  , ssn           VARCHAR(11)     NOT NULL    UNIQUE
+  , employeeName  VARCHAR(100)    NOT NULL
   , supervisor    INT
   , wage          DECIMAL(20, 2)
   , address       VARCHAR(100)
   , phone         VARCHAR(50)
   , shiftCode     INT
-  , PRIMARY KEY(employeeId)
+  , PRIMARY KEY (employeeId)
+  , FOREIGN KEY (supervisor) REFERENCES STAFF (employeeId)
+  , FOREIGN KEY (shiftCode) REFERENCES SHIFT (code)
 );
 
-CREATE TABLE DOCTOR(
-    employeeId    INT             REFERENCES STAFF(employeeId)
+CREATE TABLE DOCTOR (
+    employeeId    INT             NOT NULL    REFERENCES STAFF (employeeId)
   , position      VARCHAR(100)
-  , onCall        BIT
-  , PRIMARY KEY(employeeId)
+  , onCall        BIT             NOT NULL
+  , PRIMARY KEY (employeeId)
 );
 
-CREATE TABLE SURGEON(
-    employeeId    INT             REFERENCES DOCTOR(employeeId)
+CREATE TABLE SURGEON (
+    employeeId    INT             NOT NULL    REFERENCES DOCTOR (employeeId)
   , surgeonType   VARCHAR(100)
-  , PRIMARY KEY(employeeId)
+  , PRIMARY KEY (employeeId)
 );
 
-CREATE TABLE GENERAL(
-    employeeId    INT             REFERENCES DOCTOR(employeeId)
+CREATE TABLE GENERAL (
+    employeeId    INT             NOT NULL    REFERENCES DOCTOR (employeeId)
   , type          VARCHAR(100)
-  , PRIMARY KEY(employeeId)
+  , PRIMARY KEY (employeeId)
 );
 
-CREATE TABLE SPECIALIST(
-    employeeId    INT             REFERENCES DOCTOR(employeeId)
+CREATE TABLE SPECIALIST (
+    employeeId    INT             NOT NULL    REFERENCES DOCTOR (employeeId)
   , specialtyType   VARCHAR(100)
-  , PRIMARY KEY(employeeId)
+  , PRIMARY KEY (employeeId)
 );
 
-CREATE TABLE NURSE(
-    employeeId    INT             REFERENCES STAFF(employeeId)
+CREATE TABLE NURSE (
+    employeeId    INT             NOT NULL    REFERENCES STAFF (employeeId)
   , position      VARCHAR(100)
-  , isRegistered  BIT
-  , credentials   VARCHAR(100)
-  , onCall        BIT
-  , PRIMARY KEY(employeeId)
+  , isRegistered  BIT             NOT NULL
+  , credentials   VARCHAR(100)    NOT NULL
+  , onCall        BIT             NOT NULL
+  , PRIMARY KEY (employeeId)
 );
 
-CREATE TABLE PATIENT(
-    patientId     INT
-  , ssn           VARCHAR(11)
-  , name          VARCHAR(100)
+CREATE TABLE PATIENT (
+    patientId     INT           NOT NULL
+  , ssn           VARCHAR(11)   NOT NULL    UNIQUE
+  , name          VARCHAR(100)  NOT NULL
   , address       VARCHAR(100)
   , phone         VARCHAR(50)
   , insuranceId   INT
   , pcp           VARCHAR(100)
-  , PRIMARY KEY(patientId)
+  , PRIMARY KEY (patientId)
 );
 
-CREATE TABLE SHIFT(
-    code        INT
+CREATE TABLE SHIFT (
+    code        INT           NOT NULL
   , startTime   TIME
   , endTime     TIME
   , days        VARCHAR(100)
-  , PRIMARY KEY(code)
+  , PRIMARY KEY (code)
 );
 
-CREATE TABLE PROCEDURE(
-    code  INT
-  , name  VARCHAR(250)
+CREATE TABLE MPROCEDURE (
+    code  INT               NOT NULL
+  , name  VARCHAR(250)      NOT NULL
   , cost  DECIMAL(100, 2)
-  , PRIMARY KEY(code)
+  , PRIMARY KEY (code)
 );
 
-CREATE TABLE OUTPATIENTPROCEDURE(
-    code  INT   REFERENCES PROCEDURE(code)
-  , PRIMARY KEY(code)
+CREATE TABLE OUTPATIENTPROCEDURE (
+    code  INT   NOT NULL  REFERENCES PROCEDURE (code)
+  , PRIMARY KEY (code)
 );
 
-CREATE TABLE INPATIENTPROCEDURE(
-    code            INT             REFERENCES PROCEDURE(code)
-  , avgStayLength   DECIMAL(10, 2)
-  , PRIMARY KEY(code)
+CREATE TABLE INPATIENTPROCEDURE (
+    code         INT              NOT NULL   REFERENCES PROCEDURE (code)
+  , avgStayLen   DECIMAL(100, 2)
+  , PRIMARY KEY (code)
 );
 
-CREATE TABLE DEPARTMENT(
-    departmentId  INT
+CREATE TABLE DEPARTMENT (
+    departmentId  INT           NOT NULL
   , name          VARCHAR(150)
   , head          INT
-  , location      VARCHAR(100)
-  , PRIMARY KEY(departmentId)
-  , FOREIGN KEY (head) references STAFF(employeeId)
+  , location      VARCHAR(100)  NOT NULL
+  , PRIMARY KEY (departmentId)
+  , FOREIGN KEY (head) references STAFF (employeeId)
 );
 
-CREATE TABLE BUILDING(
-    buildingId  INT
-  , address     VARCHAR(100)
+CREATE TABLE LOCATION (
+    locationId  INT           NOT NULL
+  , address     VARCHAR(100)  NOT NULL
   , name        VARCHAR(150)
-  , wing        VARCHAR(150)
-  , PRIMARY KEY(buildingId)
+  , PRIMARY KEY (locationId)
 );
 
-CREATE TABLE ROOM(
-    roomNumber    INT
+CREATE TABLE ROOM (
+    roomNumber    VARCHAR(20)   NOT NULL
   , roomType      VARCHAR(100)
-  , floor         INT
-  , availability  BIT
-  , PRIMARY KEY(roomNumber)
+  , floor         INT           NOT NULL
+  , availability  BIT           NOT NULL
+  , PRIMARY KEY (roomNumber)
 );
 
-CREATE TABLE MEDICATION(
-    code    INT
-  , name    VARCHAR(150)
-  , brand   VARCHAR(150)
+CREATE TABLE MEDICATION (
+    code    INT               NOT NULL
+  , name    VARCHAR(150)      NOT NULL
+  , brand   VARCHAR(150)      NOT NULL
   , cost    DECIMAL(100, 2)
-  , PRIMARY KEY(code)
+  , PRIMARY KEY (code)
 );
 
-CREATE TABLE DIAGNOSIS(
-    diagnosisName   VARCHAR(250)
+CREATE TABLE DIAGNOSIS (
+    diagnosisName   VARCHAR(250)    NOT NULL
   , symptoms        VARCHAR(1000)
-  , PRIMARY KEY(diagnosisName)
+  , PRIMARY KEY (diagnosisName)
 );
 
+CREATE TABLE WORKSIN (
+    employeeId    INT   NOT NULL
+  , departmentId  INT   NOT NULL
+  , PRIMARY KEY (employeeId, departmentId)
+  , FOREIGN KEY (employeeId) REFERENCES STAFF (employeeId)
+  , FOREIGN KEY (departmentId) REFERENCES DEPARTMENT (departmentId)
+);
 
+CREATE TABLE WORKSWITH (
+    doctorId  INT   NOT NULL
+  , nurseId   INT   NOT NULL
+  , PRIMARY KEY (doctorId, nurseId)
+  , FOREIGN KEY (doctorId) REFERENCES DOCTOR (employeeId)
+  , FOREIGN KEY (nurseId) REFERENCES NURSE (employeeId)
+);
+
+CREATE TABLE PRESCRIBES (
+    employeeId      INT         NOT NULL
+  , patientId       INT         NOT NULL
+  , code            INT         NOT NULL
+  , datePrescribed  DATE        NOT NULL
+  , dosage          DECIMAL     NOT NULL
+  , measurementType VARCHAR(20) NOT NULL
+  , PRIMARY KEY (employeeId, patientId, code, datePrescribed)
+  , FOREIGN KEY (employeeId) REFERENCES DOCTOR (employeeId)
+  , FOREIGN KEY (patientId) REFERENCES PATIENT (patientId)
+  , FOREIGN KEY (code) REFERENCES MEDICATION (code)
+);
+
+CREATE TABLE HOUSES (
+    locationId  INT           NOT NULL
+  , roomNumber  VARCHAR(20)   NOT NULL
+  , PRIMARY KEY (locationId, roomNumber)
+  , FOREIGN KEY (locationId) REFERENCES LOCATION (locationId)
+  , FOREIGN KEY (roomNumber) REFERENCES ROOM (roomNumber)
+);
+
+CREATE TABLE ISSCHEDULED (
+    employeeId  INT   NOT NULL
+  , code        INT   NOT NULL
+  , PRIMARY KEY (employeeId, code)
+  , FOREIGN KEY (employeeId) REFERENCES STAFF (employeeId)
+  , FOREIGN KEY (code) REFERENCES SHIFT (code)
+);
+
+CREATE TABLE LOCATEDIN (
+    departmentId  INT   NOT NULL
+  , locationId    INT   NOT NULL
+  , PRIMARY KEY (departmentId, locationId)
+  , FOREIGN KEY (departmentId) REFERENCES DEPARTMENT (departmentId)
+  , FOREIGN KEY (locationId) REFERENCES LOCATION (locationId)
+);
+
+CREATE TABLE DIAGNOSED (
+    patientId       INT           NOT NULL
+  , diagnosisName   VARCHAR(250)  NOT NULL
+  , PRIMARY KEY (patientId, diagnosisName)
+  , FOREIGN KEY (patientId) REFERENCES PATIENT (patientId)
+  , FOREIGN KEY (diagnosisName) REFERENCES DIAGNOSIS (diagnosisName)
+);
+
+CREATE TABLE TREAT (
+    procedureCode   INT   NOT NULL
+  , medicationCode  INT   NOT NULL
+  , patientId       INT   NOT NULL
+  , employeeId      INT   NOT NULL
+  , PRIMARY KEY (procedureCode, medicationCode, patientId, employeeId)
+  , FOREIGN KEY (procedureCode) REFERENCES MPROCEDURE (code)
+  , FOREIGN KEY (medicationCode) REFERENCES MEDICATION (code)
+  , FOREIGN KEY (patientId) REFERENCES PATIENT (patientId)
+  , FOREIGN KEY (employeeId) REFERENCES DOCTOR (employeeId)
+);
+
+CREATE TABLE VISIT (
+    employeeId  INT           NOT NULL
+  , patientId   INT           NOT NULL
+  , visitDate   DATE          NOT NULL
+  , visitTime   TIME          NOT NULL
+  , notes       VARCHAR(500)
+  , PRIMARY KEY (employeeId, patientId, visitDate, visitTime)
+  , FOREIGN KEY (employeeId) REFERENCES NURSE (employeeId)
+  , FOREIGN KEY (patientId) REFERENCES PATIENT (patientId)
+);
+
+CREATE TABLE STAYS (
+    patientId
+  , roomNumber
+  , enterDate
+  , enterTime
+  , dischargeDate
+  , dischargeTime
+  , PRIMARY KEY (patientId, roomNumber, enterDate)
+  , FOREIGN KEY (patientId) REFERENCES PATIENT (patientId)
+  , FOREIGN KEY (roomNumber) REFERENCES ROOM (roomNumber)
+  , CHECK (dischargeDate >= enterDate)
+);
