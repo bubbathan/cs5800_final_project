2a33eccc18f3f61cc63a66a19be16f57ddde4f14
DuzieAmaranthine <jessycalynn9@gmail.com>
2022-12-04 19:10:29 -0700

    Updated to take authentication, will change when decision made about who can view what. Structure for keys.json added.

--- Optional/ui_main.py
+++ Optional/ui_main.py
@@ -1,5 +1,34 @@
-def welcomeMessage():
-  print("Welcome! Do you want to work with a TABLE or a FUNCTION?")
+import json
+import mysql.connector
+
+data = json.load(open('./Optional/keys.json'))
+mysql.connector.connect(host=data['host'], 
+                        user=data['user'], 
+                        password=data['password'], 
+                        database=data['database']
+                        )
+
+print(data)
+
+
+# TODO: Get all staff id and put into a list, or lists
+# TODO: Check which list the user id is in to determine navigation
+def authUser():
+  print("Welcome, please enter your System ID")
+  x = input().upper()
+  if x == "DOCTOR":
+    print("Welcome, Doctor.")
+    home()
+  elif x == "NURSE":
+    print("Welcome, Nurse.")
+    home()
+  else:
+    print("Invalid System ID. Goodbye.")
+    exit()
+
+
+def home():
+  print("Do you want to work with a TABLE or a FUNCTION?")
   x = input().upper()
   print("You have chosen to work with a " + x + ".")
   if x == "TABLE":
@@ -8,24 +37,28 @@ def welcomeMessage():
     functionInput()
   else:
     print("Please enter a valid input.")
-    welcomeMessage()
+    home()
 
+
+# TODO: Print table requested
+# TODO: List options for each table
 def tableInput():
   print("Please enter the name of the TABLE you want to work with or RETURN.")
   print("[TableList]")
   x = input().upper()
   if x == "RETURN":
-    welcomeMessage()
+    home()
   else:
     print("You have chosen to work with the " + x + " TABLE.")
 
+# TODO: Connect to functions and run the chosen one
 def functionInput():
   print("Please enter the name of the FUNCTION you want to work with or RETURN.")
   print("[FunctionList]")
   x = input().upper()
   if x == "RETURN":
-    welcomeMessage()
+    home()
   else:
     print("You have chosen to work with the " + x + " FUNCTION.")
 
-welcomeMessage()
+authUser()
