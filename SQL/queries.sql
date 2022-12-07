8d6cc42154c12a4825fb7c5f2aa6a79befab8d8c
Christian Allen <chin.allen9@hotmail.com>
2022-12-06 21:20:53 -0700

    Finished writing and testing register_patient, patient_report, schedule_staff procedures.

--- SQL/queries.sql
+++ SQL/queries.sql
@@ -1,50 +1,51 @@
+USE HOSPITAL;
+
+DROP PROCEDURE IF EXISTS register_patient;
+DROP PROCEDURE IF EXISTS patient_report;
+DROP PROCEDURE IF EXISTS schedule_staff;
+
 DELIMITER $$
 CREATE PROCEDURE register_patient (IN patientId INT,
-                                    IN name VARCHAR (100), 
+                                    IN pName VARCHAR (100), 
                                     IN ssn VARCHAR (11), 
                                     IN address VARCHAR (100), 
                                     IN phone VARCHAR (50), 
                                     IN insuranceId INT, 
                                     IN pcp VARCHAR (100)
-                                    OUT patientId INT)
+                                    )
 begin
-    insert into PATIENT (patientId, name, ssn, address, phone, insuranceId, pcp) 
-    values (patientId, name, ssn, address, phone, insuranceId, pcp);
-    set patientId = last_insert_id();
+	insert into PATIENT (patientId, name, ssn, address, phone, insuranceId, pcp) 
+	values (patientId, pName, ssn, address, phone, insuranceId, pcp);
 end$$
 DELIMITER ;
 
 DELIMITER $$
-CREATE PROCEDURE patient_report (IN patientId INT,
-                                OUT name VARCHAR (100),
+CREATE PROCEDURE patient_report (IN pId INT,
+                                OUT pName VARCHAR (100),
                                 OUT phone VARCHAR (50),
                                 OUT bill DECIMAL (20, 2),
                                 OUT procedures VARCHAR (1000),
-                                OUT prescriptions VARCHAR (1000))
+                                OUT prescriptions VARCHAR (1000)
+                                )
 begin
-    select name, phone into name, phone from PATIENT where patientId = patientId;
-    select sum(cost) into bill from BILL where patientId = patientId;
-    select group_concat(name) into procedures from MPROCEDURE where code in (select procedureCode from TREAT where patientId = patientId); 
-    select group_concat(name) into prescriptions from MEDICATION where code in (select medicationCode from PRESCRIBE where patientId = patientId);
+	select name, phone into pName, phone from PATIENT where patientId = pId;
+	select sum(cost) into bill from MPROCEDURE where code in (select code from TREAT where patientId = pId);
+	select group_concat(name) into procedures from MPROCEDURE where code in (select procedureCode from TREAT where patientId = pId); 
+	select group_concat(name) into prescriptions from MEDICATION where code in (select code from PRESCRIBES where patientId = pId);
+
+	select pName, phone, bill, procedures, prescriptions;
+
 end$$
 DELIMITER ;
 
 DELIMITER $$
-CREATE PROCEDURE schedule_staff(IN employeeName VARCHAR (100),
+CREATE PROCEDURE schedule_staff (IN eName VARCHAR (100),
                                 IN code INT,
-                                IN shiftDate DATE,
-                                OUT employeeName VARCHAR (100),
-                                OUT shiftDate Date)
+                                IN shiftDate DATE
+                                )
 begin
-    declare tempId INT;
-    select employeeId into tempId from STAFF where employeeName = employeeName;
+	select employeeId into @eId from STAFF where employeeName = eName;
 
-    insert into ISSCHEDULED (employeeId, code, shiftDate) values (tempId, code, shiftDate);
-    set employeeName = employeeName;
-    set shiftDate = shiftDate;
+	insert into ISSCHEDULED (employeeId, code, scheduledDate) values (@eId, code, shiftDate);
 end$$
 DELIMITER ;
-
-
-
-
