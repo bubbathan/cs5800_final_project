408360daf43724d428c07affa64af6c068ba7797
Christian Allen <chin.allen9@hotmail.com>
2022-12-09 14:41:41 -0700

    Added UI functionality for the remaining procedures and updated the README on UI instruction.

--- Optional/ui_functions.py
+++ Optional/ui_functions.py
@@ -53,12 +53,11 @@ Generates a patient report which includes:
     - Patient procedures
     - Patient prescriptions
 Input:
-    db: database object
     cursor: cursor object
 
 author : skal-chin
 '''
-def patient_report(db, cursor):
+def patient_report(cursor):
 
     cursor.execute('SELECT patientId FROM patient')
     patientIds = [patient[0] for patient in cursor.fetchall()]
@@ -105,6 +104,150 @@ def schedule_staff(db, cursor):
     db.commit()
 
     print('New staff member has been scheduled')
+    print(results)
+
+    return
+
+'''
+Shows the available rooms
+Input:
+    cursor: cursor object
+
+author : skal-chin
+'''
+def get_available_rooms(cursor):
+
+    results = cursor.callproc('get_available_rooms', ['@rooms'])
+    print(results)
+
+    return
+
+'''
+Retrieves all of the information on medications
+Input:
+    cursor: cursor object
+
+author : skal-chin
+'''
+def get_medications(cursor):
+
+    results = cursor.callproc('get_medications', ['@medications'])
+    print(results)
+
+    return
+
+'''
+Retrieves all of the doctors currently on-call
+Input:
+    cursor: cursor object
+
+author : skal-chin
+'''
+
+def get_on_call_doctors(cursor):
+    
+    results = cursor.callproc('get_on_call_doctors', ['@doctors'])
+    print(results)
+
+    return
+
+'''
+Changes the cost of a medication
+Input:
+    db: database object
+    cursor: cursor object
+
+author : skal-chin
+'''
+def change_medication_cost(db, cursor):
+    
+    print('Input medication name (REQUIRED)')
+    medicationName = input()
+
+    print('Input new cost (REQUIRED)')
+    cost = input()
+
+    cursor.execute(f'SELECT code FROM medication WHERE name = "{medicationName}"')
+    medicationCode = cursor.fetchone()[0]
+
+    results = cursor.callproc('change_medication_cost', [medicationCode, cost])
+    db.commit()
+
+    print('Medication cost has been changed')
+    print(results)
+
+    return
+
+'''
+Changes the cost of a procedure
+Input:
+    db: database object
+    cursor: cursor object
+
+author : skal-chin
+'''
+def change_procedure_cost(db, cursor):
+        
+    print('Input procedure name (REQUIRED)')
+    procedureName = input()
+
+    print('Input new cost (REQUIRED)')
+    cost = input()
+
+    cursor.execute(f'SELECT code FROM mprocedure WHERE name = "{procedureName}"')
+    procedureCode = cursor.fetchone()[0]
+
+    results = cursor.callproc('change_procedure_cost', [procedureCode, cost])
+    db.commit()
+
+    print('Procedure cost has been changed')
+    print(results)
+
+    return
+
+'''
+Updates the patient information
+Input:
+    db: database object
+    cursor: cursor object
+
+author : skal-chin
+'''
+def update_patient_info(db, cursor):
+
+    print('Input patient id (REQUIRED)')
+    patientId = input()
+
+    print('Input new patient name (REQUIRED)')
+    name = input()
+
+    print('Input new patient ssn (REQUIRED)')
+    ssn = input()
+
+    print('Input new patient address (OPTIONAL)')
+    address = input()
+    if not address:
+        address = None
+
+    print('Input new patient phone (OPTIONAL)')
+    phone = input()
+    if not phone:
+        phone = None
+
+    print('Input new patient insurance id (OPTIONAL)')
+    insuranceId = input()
+    if not insuranceId:
+        insuranceId = None
+
+    print('Input new patient pcp (OPTIONAL)')
+    pcp = input()
+    if not pcp:
+        pcp = None
+
+    results = cursor.callproc('update_patient_info', [patientId, name, ssn, address, phone, insuranceId, pcp])
+    db.commit()
+
+    print('Patient information has been updated')
     print(results)
 
     return
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # cs5800_final_project
 CS5800 final project
+
+### Running the UI
+
+In order for the UI to work, you will need to copy 'example_keys.json' into 'keys.json' and fill in the appropriate values.
+
+You will also need the package 'mysql-connector-python' installed.
+
+Before the application is run, you will need to run the following files into a MySQL terminal:
+
+- create.sql
+- insert.sql
+- queries.sql
+
+They must be run in that order. Once the database is seeded, the application can be run and you will see a 'login' asking for an ID number. For testing purposes, you can use the ID number '1' to log in. From there, you can navigate the UI by following the prompts.
+
+**NOTE** The UI is not fully functional. There will be some values and inputs that break the program.
